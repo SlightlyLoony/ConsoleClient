7759d8af78bc1391de50e3e14385cb7f2d9403f1
Tom Dilatush <tom@dilatush.com>
2021-02-14 11:03:57 -0700

    Finally!  I actually DID fix the problem with reading from System.in not being interruptable.  The key was a simple one: run the input reader in a daemon thread.

--- src/com/dilatush/consoleclient/ConsoleClient.java
+++ src/com/dilatush/consoleclient/ConsoleClient.java
@@ -15,6 +15,7 @@ import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Semaphore;
 
 import static com.dilatush.util.General.isNull;
 import static com.dilatush.util.Sockets.close;
@@ -31,10 +32,10 @@ public class ConsoleClient {
     private static final int MAJOR_VERSION = 1;
     private static final int MINOR_VERSION = 0;
 
-    private static BufferedReader keyReader = null;
-    private static BufferedReader reader;
-    private static Socket socket;
-    private static Thread main;
+    private static BufferedReader  reader;
+    private static BufferedWriter  writer;
+    private static Socket          socket;
+    private static Semaphore       waiter;
 
 
     /**
@@ -44,8 +45,111 @@ public class ConsoleClient {
      */
     public static void main( final String[] _args ) {
 
-        main = Thread.currentThread();
+        // process our command line and get our server configuration...
+        Config.Server server = getServerConfig( _args );
+        if( server == null )
+            return;
 
+        try {
+
+            // connect to the target Console Server...
+            connectToServer( server );
+
+            // get the semaphore we'll wait on before exiting the program...
+            waiter = new Semaphore( 0 );
+
+            // get a nice executor and start two daemon threads: one for reading the network, and the other for reading the keyboard...
+            ExecutorService executor = new ExecutorService( 2 );
+            executor.submit( ConsoleClient::readFromServer );
+            executor.submit( ConsoleClient::readKeyboard );
+
+            // wait here until either the network reader or the keyboard reader terminates us...
+            waiter.acquire();
+        }
+        catch( IOException _e  ) {
+            out( "Problem connecting to Console Server: " + _e.getMessage() );
+        }
+        catch( InterruptedException _e ) {
+            out( "We was interrupted!" );
+        }
+
+        out( "Our work is done here..." );
+    }
+
+
+    /**
+     * Runnable to read from the keyboard and write to the console server...
+     */
+    private static void readKeyboard() {
+
+        BufferedReader keyReader = new BufferedReader( new InputStreamReader( System.in ) );
+
+        try {
+            while( !Thread.currentThread().isInterrupted() ) {
+
+                String line = keyReader.readLine();
+                if( isNull( line ) )
+                    break;
+                writer.write( line );
+                writer.newLine();
+                writer.flush();
+            }
+        }
+        catch( IOException _e ) {
+            out( "Problem reading from keyboard or writing to network: " + _e.getMessage() );
+        }
+
+        // release the main thread, to exit the program...
+        waiter.release();
+    }
+
+
+    /**
+     * Establish an encrypting TCP connection to the console server.
+     *
+     * @param server The configuration for the server (and console) that we want to connect to.
+     * @throws IOException on any I/O problem
+     */
+    private static void connectToServer( final Config.Server server ) throws IOException {
+
+        socket = new Socket( server.socket.getAddress(), server.socket.getPort() );
+
+        // get the server ID and make sure all is ok...
+        ID id = getID( socket );
+        if( !server.serverName.equals( id.name ) )
+            throw new IOException( "Invalid server name: got '" + id.name + "', expected '" + server.serverName + "'" );
+        if( (id.major > MAJOR_VERSION) || (id.minor > MINOR_VERSION) )
+            throw new IOException( "Incompatible version: " + id.major + "." + id.minor );
+
+        // get our encrypting output stream and turn it into a buffered writer...
+        Key key = Crypto.getKey_AES_128( server.secret );
+        CipherOutputStream cos = Crypto.getSocketOutputStream_AES_128_CTR( socket, key );
+        OutputStreamWriter osw = new OutputStreamWriter( cos, StandardCharsets.UTF_8 );
+        writer = new BufferedWriter( osw, 1000 );
+
+        // send the console name to the server...
+        writer.write( server.console + "\n" );
+        writer.flush();
+
+        // get our decrypting input stream and turn it into a buffered reader...
+        CipherInputStream cis = Crypto.getSocketInputStream_AES_128_CTR( socket, key );
+        InputStreamReader isr = new InputStreamReader( cis, StandardCharsets.UTF_8 );
+        reader = new BufferedReader( isr, 1000 );
+
+        // get the console server's response
+        String serverResponse = reader.readLine();
+        if( !"OK".equals( serverResponse ) )
+            throw new IOException( "Invalid server response received: " + serverResponse );
+    }
+
+
+    /**
+     * Get the configuration for the console server (and console) that the user wants to connect to.
+     *
+     * @param _args The command line arguments.
+     * @return the desired configuration
+     */
+    private static Config.Server getServerConfig( final String[] _args ) {
         // get our parsed command line - valid and no help...
         ParsedCommandLine result = CmdLine.get( _args );
 
@@ -59,7 +163,7 @@ public class ConsoleClient {
                 SecretKey newKey = keygen.generateKey();
 
                 // print it out in base 64 form...
-                System.out.println( "Key: " + Base64Fast.encode( newKey.getEncoded() ) );
+                out( "Key: " + Base64Fast.encode( newKey.getEncoded() ) );
 
                 // leave...
                 System.exit( 0 );
@@ -67,15 +171,15 @@ public class ConsoleClient {
             catch( NoSuchAlgorithmException _e ) {
 
                 // if this happens, then something really weird is going on...
-                System.out.println( "(Very) unexpected crypto problem: " + _e.getMessage() );
+                out( "(Very) unexpected crypto problem: " + _e.getMessage() );
                 System.exit( 1 );
             }
         }
 
         // if we have no config, complain...
         if( result.getValue( "config" ) == null ) {
-            System.out.println( "No configuration at <user>/.console/config.js, and none specified.");
-            return;
+            out( "No configuration at <user>/.console/config.js, and none specified.");
+            return null;
         }
 
         // get our configuration...
@@ -84,76 +188,20 @@ public class ConsoleClient {
         // if we didn't get a name, list what we've configured...
         if( !result.get( "name" ).present ) {
 
-            System.out.println( "No connection specified" );
+            out( "No connection specified" );
             showServers( config );
-            return;
+            return null;
         }
 
         // we weren't doing anything else, so try to establish a connection to the requested server...
         String name = (String) result.getValue( "name" );
         Config.Server server = config.servers.get( name );
         if( server == null ) {
-            System.out.println( "Server is not configured: " + name );
+            out( "Server is not configured: " + name );
             showServers( config );
-            return;
+            return null;
         }
-
-        try {
-            socket = new Socket( server.socket.getAddress(), server.socket.getPort() );
-
-            // get the server ID and make sure all is ok...
-            ID id = getID( socket );
-            if( !server.serverName.equals( id.name ) )
-                throw new IOException( "Invalid server name: got '" + id.name + "', expected '" + server.serverName + "'" );
-            if( (id.major > MAJOR_VERSION) || (id.minor > MINOR_VERSION) )
-                throw new IOException( "Incompatible version: " + id.major + "." + id.minor );
-
-            // get our encrypting output stream and turn it into a buffered writer...
-            Key key = Crypto.getKey_AES_128( server.secret );
-            CipherOutputStream cos = Crypto.getSocketOutputStream_AES_128_CTR( socket, key );
-            OutputStreamWriter osw = new OutputStreamWriter( cos, StandardCharsets.UTF_8 );
-            BufferedWriter writer = new BufferedWriter( osw, 1000 );
-
-            // send the console name to the server...
-            writer.write( server.console + "\n" );
-            writer.flush();
-
-            // get our decrypting input stream and turn it into a buffered reader...
-            CipherInputStream cis = Crypto.getSocketInputStream_AES_128_CTR( socket, key );
-            InputStreamReader isr = new InputStreamReader( cis, StandardCharsets.UTF_8 );
-            reader = new BufferedReader( isr, 1000 );
-
-            // get the console server's response
-            String serverResponse = reader.readLine();
-            if( !"OK".equals( serverResponse ) )
-                throw new IOException( "Invalid server response received: " + serverResponse );
-
-            // if we make it to here, we've got a bidirectional encrypted connection set up with our buffered reader and writer...
-
-            // start up our input reader thread...
-            Thread reader = new Thread( new InputReader() );
-            reader.setDaemon( true );
-            reader.start();
-
-            // loop while getting user input...
-            keyReader = new BufferedReader( new InputStreamReader( System.in ) );
-            while( !Thread.currentThread().isInterrupted() ) {
-                String line = keyReader.readLine();
-                if( isNull( line ) )
-                    break;
-                writer.write( line );
-                writer.newLine();
-                writer.flush();
-            }
-        }
-        catch( IOException _e  ) {
-            System.out.println( _e.getMessage() );
-        }
-        finally {
-            close( socket );
-        }
-
-        System.out.println( "Our work is done here..." );
+        return server;
     }
 
 
@@ -163,54 +211,33 @@ public class ConsoleClient {
      * @param _config The configuration object.
      */
     private static void showServers( final Config _config ) {
-        System.out.println( "Configured server connections available:");
+        out( "Configured server connections available:");
         _config.servers.forEach(
-                (name,server) -> System.out.println( "  " + name + ":  " + server.host + ":" + server.port + " --> " + server.console )
+                (name,server) -> out( "  " + name + ":  " + server.host + ":" + server.port + " --> " + server.console )
         );
     }
 
 
     /**
-     * A simple socket reader that reads from the decrypting input reader.
+     * Read from the console server and write to stdout.
      */
-    private static class InputReader implements Runnable {
+    public static void readFromServer() {
 
-        /**
-         * When an object implementing interface <code>Runnable</code> is used to create a thread, starting the thread causes the object's
-         * <code>run</code> method to be called in that separately executing
-         * thread.
-         * <p>
-         * The general contract of the method <code>run</code> is that it may take any action whatsoever.
-         *
-         * @see Thread#run()
-         */
-        @Override
-        public void run() {
-
-            while( !Thread.currentThread().isInterrupted() ) {
-                try {
-                    String line = reader.readLine();
-                    if( isNull( line ) )
-                        break;
-                    System.out.println( line );
-                }
-                catch( IOException _e ) {
-                    System.out.println( "Error while reading from console server: " + _e.getMessage() );
-                    close( socket );
-                }
+        while( !Thread.currentThread().isInterrupted() ) {
+            try {
+                String line = reader.readLine();
+                if( isNull( line ) )
+                    break;
+                out( line );
             }
-
-            // stop our key reader if we possibly can...
-            main.interrupt();
-            if( ConsoleClient.keyReader != null ) {
-                try {
-                    keyReader.close();
-                }
-                catch( IOException _e ) {
-                    // naught to do, just ignore it...
-                }
+            catch( IOException _e ) {
+                out( "Error while reading from console server: " + _e.getMessage() );
+                close( socket );
             }
         }
+
+        // time to release the main thread, to exit...
+         waiter.release();
     }
 
 
@@ -312,5 +339,15 @@ public class ConsoleClient {
             public String console;            // the name of the console provider to connect to (as configured on the console server)
             public InetSocketAddress socket;  // the socket to connect to; created from host and port by configuration validation
         }
+    }
+
+
+    /**
+     * Output a message (on a line) to stdout.
+     *
+     * @param _msg The message to send to stdout.
+     */
+    private static void out( final String _msg ) {
+        System.out.println( _msg );
     }
 }
